946f99c1ec4b44ebaac6d2065097be349a3cd9c0
Pablo Sousa <pablosousa.ads@gmail.com>
2019-10-24 22:57:54 -0300

    feature: criado recurso de cadastro de usuario

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -13,6 +14,12 @@ class User extends Model {
         sequelize,
       }
     );
+
+    this.addHook('beforeCreate', async user => {
+      user.password = await bcrypt.hash(user.password, 8);
+    });
+
+    return this;
   }
 }
 
