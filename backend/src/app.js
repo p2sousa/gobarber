41b1ab23d7e244f2375da758a1a118cf4609f94f
Pablo Sousa <pablosousa.ads@gmail.com>
2019-11-16 12:43:20 -0300

    adicionado criacao de appointments.

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,9 +14,7 @@ class App {
     this.server.use(express.json());
     this.server.use(
       '/files',
-      express.static(
-        path.resolve(__dirname, '..', 'tmp', 'uploads')
-      )
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
   }
 
--- backend/src/app/controllers/ProviderController.js
+++ backend/src/app/controllers/ProviderController.js
@@ -1,19 +1,18 @@
-import * as Yup from 'yup';
 import User from '../models/User';
 import File from '../models/File';
 
 class ProviderController {
   async index(req, res) {
     const providers = await User.findAll({
-      where: {provider: true},
+      where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
       include: [
         {
           model: File,
           as: 'avatar',
-          attributes: ['name', 'path', 'url']
-        }
-      ]
+          attributes: ['name', 'path', 'url'],
+        },
+      ],
     });
 
     return res.json(providers);
