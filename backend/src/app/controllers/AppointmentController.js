7435378aedcd67a97136af7b7b80ed514bde643c
Pablo Sousa <pablosousa.ads@gmail.com>
2019-11-19 21:34:39 -0300

    criado notificacoes de agendamento

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,12 +1,14 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notifications';
 
 class AppointmentController {
   async index(req, res) {
-    const { page = 1} = req.query;
+    const { page = 1 } = req.query;
 
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
@@ -24,10 +26,10 @@ class AppointmentController {
               model: File,
               as: 'avatar',
               attributes: ['id', 'path', 'url'],
-            }
-          ]
-        }
-      ]
+            },
+          ],
+        },
+      ],
     });
 
     return res.json(appointments);
@@ -62,25 +64,41 @@ class AppointmentController {
     const hourStart = startOfHour(parseISO(date));
 
     if (isBefore(hourStart, new Date())) {
-      return res.status(400).json({error: 'Past dates are not permitted'});
+      return res.status(400).json({ error: 'Past dates are not permitted' });
     }
 
     const checkAvailability = await Appointment.findOne({
       where: {
         provider_id,
         canceled_at: null,
-        date: hourStart
-      }
+        date: hourStart,
+      },
     });
 
     if (checkAvailability) {
-      return res.status(400).json({error: 'Appointment date is not availability'});
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not availability' });
     }
 
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    /**
+     * Notify appointment a provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+    await Notification.create({
+      content: `Novo agendamento do ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
--- backend/src/app/controllers/ScheduleController.js
+++ backend/src/app/controllers/ScheduleController.js
@@ -1,20 +1,21 @@
-import { startOfDay, endOfDay, parseISO} from 'date-fns';
-import {Op} from 'sequelize';
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class ScheduleController {
   async index(req, res) {
-    const { page = 1, date} = req.query;
+    const { page = 1, date } = req.query;
     const parseDate = parseISO(date);
 
     const checkUserProvider = User.findOne({
-      where: {id: req.userId, provider: true}
+      where: { id: req.userId, provider: true },
     });
 
     if (!checkUserProvider) {
-      return res.status(401).json({ error: 'User is not a provider.'});
+      return res.status(401).json({ error: 'User is not a provider.' });
     }
 
     const appointments = await Appointment.findAll({
@@ -22,11 +23,8 @@ class ScheduleController {
         provider_id: req.userId,
         canceled_at: null,
         date: {
-          [Op.between]: [
-            startOfDay(parseDate),
-            endOfDay(parseDate),
-          ]
-        }
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
       },
       order: ['date'],
       limit: 20,
@@ -41,10 +39,10 @@ class ScheduleController {
               model: File,
               as: 'avatar',
               attributes: ['id', 'path', 'url'],
-            }
-          ]
-        }
-      ]
+            },
+          ],
+        },
+      ],
     });
 
     return res.json(appointments);
