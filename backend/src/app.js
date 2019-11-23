4c9c38ad22a6698f5e25b4786689b64545901fb8
Pablo Sousa <pablosousa.ads@gmail.com>
2019-11-23 20:35:29 -0300

    adicionado configuracoes de seguranca

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,11 @@ import './bootstrap';
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import routes from './routes';
@@ -19,10 +24,28 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(
+      cors({
+        origin: process.env.FRONT_URL,
+      })
+    );
+    this.server.use(helmet());
     this.server.use(express.json());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
+    this.server.use(
+      new RateLimit({
+        store: new RateLimitRedis({
+          client: redis.createClient({
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+          }),
+        }),
+        windowMs: 1000 * 60 * 15, // 15min
+        max: 100,
+      })
     );
   }
 
@@ -37,7 +60,7 @@ class App {
         const errors = new Youch(err, req).toJSON();
         return res.status(500).json(errors);
       }
-      return res.status(500).json({error: 'Internal Server Error'})
+      return res.status(500).json({ error: 'Internal Server Error' });
     });
   }
 }
